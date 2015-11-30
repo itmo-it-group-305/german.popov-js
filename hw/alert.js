a945d682da711ab9ccc1caaf024961572dae1ad1
gerhdev <gpop92@me.com>
2015-11-30 18:49:15 +0300

    добавил alert

---
+++ hw/alert.js
@@ -0,0 +1,30 @@
+/**
+ * Created by gerh on 29.11.15.
+ */
+//alert('I - Java Script')
+//alert('Привет')
+//alert('мир')
+//alert(3 +
+//1
+//+2);
+
+//Переменные
+
+//var name = "Василий";
+//var admin;
+//admin = name; // присвоили значение admin
+//alert (admin);
+
+
+// Выбор переменной
+
+//var earth; usName;
+//earth = "Планета земля";
+//usName = "Петя"
+//alert(earth);
+//alert(usName);
+
+// Шесть типов данных, typeof
+
+// Основные операторы
+
